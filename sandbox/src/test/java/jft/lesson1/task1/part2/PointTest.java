c8464b98d24faefb7d2f56d30225116252493035
SomeVoices <89222561+SomeVoices@users.noreply.github.com>
2021-08-25 16:48:14 +0300

    Added two tests

---
+++ sandbox/src/test/java/jft/lesson1/task1/part2/PointTest.java
@@ -0,0 +1,23 @@
+package jft.lesson1.task1.part2;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTest {
+
+  @Test
+  public void identicalPointTest (){
+    Point p1 = new Point(5, 5);
+    Point p2 = new Point(5, 5);
+
+    Assert.assertEquals(p1.distance(p2), 0);
+  }
+
+  @Test
+  public void negativeCoordinatesTest (){
+    Point p1 = new Point(2, 1);
+    Point p2 = new Point(-2, -2);
+
+    Assert.assertEquals(p1.distance(p2), 5);
+  }
+}
